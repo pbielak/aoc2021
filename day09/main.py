46eb4af530daa84fe7d1cdcd5e8de623a4dab281
Piotr Bielak <piotrbielak2@gmail.com>
2021-12-09 15:01:08 +0100

    Day 09 - part two

--- day09/main.py
+++ day09/main.py
@@ -1,10 +1,12 @@
 """Day 9 - Advent of Code"""
 from __future__ import annotations
 
+from collections import defaultdict
 import os
 from typing import List, Tuple
 
 Data = Tuple[Tuple[int]]
+Position = Tuple[int, int]
 
 
 def read_data(filename: str) -> Data:
@@ -21,7 +23,7 @@ def get_adjacent_indices(
     y: int,
     width: int,
     height: int,
-) -> List[Tuple[int, int]]:
+) -> List[Position]:
     indices = []
 
     if x > 0:
@@ -39,9 +41,8 @@ def get_adjacent_indices(
     return indices
 
 
-def solve_part_one(data: Data) -> int:
-    total_risk_level = 0
-
+def find_low_points(data: Data) -> List[Position]:
+    low_points = []
     for i, row in enumerate(data):
         for j, value in enumerate(row):
             neighbor_indices = get_adjacent_indices(
@@ -51,14 +52,55 @@ def solve_part_one(data: Data) -> int:
                 height=len(data),
             )
             if all(value < data[y][x] for x, y in neighbor_indices):
-                risk = value + 1
-                total_risk_level += risk
+                low_points.append((i, j))
+
+    return low_points
+
+
+def solve_part_one(data: Data) -> int:
+    total_risk_level = 0
+
+    for i, j in find_low_points(data):
+        risk = data[i][j] + 1
+        total_risk_level += risk
 
     return total_risk_level
 
 
+def get_basin(low_point: Position, data: Data) -> List[Position]:
+    visited = defaultdict(bool)
+    queue = [low_point]
+
+    while queue:
+        i, j = queue.pop(0)
+        visited[(i, j)] = True
+
+        indices = get_adjacent_indices(
+            x=j, y=i, width=len(data[0]), height=len(data),
+        )
+
+        for x, y in indices:
+            if data[y][x] == 9:
+                continue
+
+            if visited[(y, x)]:
+                continue
+
+            queue.append((y, x))
+
+    basin_indices = list(visited.keys())
+    return basin_indices
+
+
 def solve_part_two(data: Data) -> int:
-    pass
+    basin_sizes = []
+    for low_point in find_low_points(data):
+        basin_indices = get_basin(low_point, data)
+        basin_sizes.append(len(basin_indices))
+
+    basin_sizes = sorted(basin_sizes)
+
+    return basin_sizes[-1] * basin_sizes[-2] * basin_sizes[-3]
 
 
 def main():
@@ -70,7 +112,7 @@ def main():
         # Test cases
         if filename == "example.txt":
             assert solve_part_one(data) == 15
-            assert solve_part_two(data) == None
+            assert solve_part_two(data) == 1134
 
         # Part 1
         solution_one = solve_part_one(data)
