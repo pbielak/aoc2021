78872aff21480b94c96e608fc4c1f1f8e28aae5f
Piotr Bielak <piotrbielak2@gmail.com>
2021-12-21 17:20:05 +0100

    Day 21 - part two

--- day21/main.py
+++ day21/main.py
@@ -1,8 +1,9 @@
 """Day 21 - Advent of Code"""
 from __future__ import annotations
 
+from functools import lru_cache
 import os
-from typing import Generator, NamedTuple
+from typing import Generator, NamedTuple, Tuple
 
 
 class Data(NamedTuple):
@@ -64,8 +65,78 @@ def solve_part_one(data: Data) -> int:
     return answer
 
 
+@lru_cache(maxsize=None)
+def count_wins_with_universe_split(
+    p1_score: int, p1_pos: int,
+    p2_score: int, p2_pos: int,
+    turn: str,
+    p1_wins: int, p2_wins: int,
+) -> Tuple[int, int]:
+    _p1_wins = p1_wins
+    _p2_wins = p2_wins
+
+    if p1_score >= 21 or p2_score >= 21:
+        return _p1_wins, _p2_wins
+
+    # The Diract die gives a single outcome of 1, 2 or 3. When rolling it three
+    # times, we get a total of:
+    # 3 - once
+    # 4 - three times
+    # 5 - six times
+    # 6 - seven times
+    # 7 - six times
+    # 8 - three times
+    # 9 - once
+    outcomes = [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]
+
+    for outcome, count in outcomes:
+        if turn == "p1":
+            _p1_pos = p1_pos + outcome
+            while _p1_pos > 10:
+                _p1_pos -= 10
+            _p1_score = p1_score + _p1_pos
+
+            if _p1_score >= 21:
+                _p1_wins += count
+                continue
+
+            _p2_score = p2_score
+            _p2_pos = p2_pos
+        else:
+            _p2_pos = p2_pos + outcome
+            while _p2_pos > 10:
+                _p2_pos -= 10
+            _p2_score = p2_score + _p2_pos
+
+            if _p2_score >= 21:
+                _p2_wins += count
+                continue
+
+            _p1_score = p1_score
+            _p1_pos = p1_pos
+
+        p1w, p2w = count_wins_with_universe_split(
+            p1_score=_p1_score, p1_pos=_p1_pos,
+            p2_score=_p2_score, p2_pos=_p2_pos,
+            turn="p1" if turn == "p2" else "p2",
+            p1_wins=p1_wins, p2_wins=p2_wins,
+        )
+        _p1_wins += p1w * count
+        _p2_wins += p2w * count
+
+    return _p1_wins, _p2_wins
+
+
 def solve_part_two(data: Data) -> int:
-    pass
+    p1_wins, p2_wins = count_wins_with_universe_split(
+        p1_score=0, p1_pos=data.player_1_starting_position,
+        p2_score=0, p2_pos=data.player_2_starting_position,
+        turn="p1",
+        p1_wins=0,
+        p2_wins=0,
+    )
+
+    return max(p1_wins, p2_wins)
 
 
 def main():
@@ -77,7 +148,7 @@ def main():
         # Test cases
         if filename == "example.txt":
             assert solve_part_one(data) == 739_785
-            assert solve_part_two(data) == None
+            assert solve_part_two(data) == 444_356_092_776_315
 
         # Part 1
         solution_one = solve_part_one(data)
